c6c6d9729fff75f895ef62fba910fae8c7d62b25
Jules <e1733070@cmaisonneuve.qc.ca>
2026-08-28 12:53:37 -0400

    fenetre additionelle pour le CPU

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ class App:
         self.fenetre = tk.Tk()
         self.fenetre.title("Monitoring système")
         self.fenetre.resizable(False, False)
-
+        
+        self.fenetre_cpu = tk.Toplevel(self.fenetre)
+        self.fenetre_cpu.title("Monitoring CPU")
+        self.label_cpu_grand = tk.Label(self.fenetre_cpu, text="0%", font=("Arial", 72, "bold"))
+        self.label_cpu_grand.pack(padx=20, pady=20)
+        
         # --- CPU ---
         self.frame_cpu = tk.LabelFrame(self.fenetre, text="CPU", padx=10, pady=10)
         self.frame_cpu.pack(fill=tk.X, padx=10, pady=5)
@@ -60,6 +65,8 @@ class App:
             self.label_80_cpu.config(text="Avertissement : 80% ou +")
         else:
             self.label_80_cpu.config(text="")
+        
+        self.label_cpu_grand.config(text=f"{cpu:.1f}%")
 
         # Mettre à jour RAM
         self.label_ram.config(text=f"{ram:.1f}%")
